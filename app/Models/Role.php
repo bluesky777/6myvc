8fbc11cbf070f42e0b4197e50c537b52854e8dd3
Joseth David <davidguerrero777@gmail.com>
2021-05-05 18:11:13 -0500

    Inicio migración

---
+++ app/Models/Role.php
@@ -0,0 +1,51 @@
+<?php namespace App\Models;
+
+use Zizaco\Entrust\EntrustRole;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+use DB;
+
+
+class Role extends EntrustRole
+{
+
+	use SoftDeletes;
+	protected $softDelete = true;
+
+	// Devolveremos los permisos del rol, detallado o solo el texto nombre.
+	public function permissions($detailed=false)
+	{
+
+		$consulta = 'SELECT pm.name, pm.display_name, pm.description from permission_role pmr
+				inner join permissions pm on pm.id = pmr.permission_id 
+					and pmr.role_id = :role_id';
+		
+		$permisos = DB::select(DB::raw($consulta), array(':role_id' => $this->id));
+		
+		$perms = array();
+
+		foreach ($permisos as $permiso) {
+			if ($detailed) {
+				array_push($perms, $permiso);
+			}else{
+				array_push($perms, $permiso->name);
+			}
+		}
+
+		return $perms;
+	}
+
+
+	public static function allConPermisos($detailed=false)
+	{
+		$roles = Role::all();
+
+		foreach ($roles as $rol) {
+			$rol->perms = $rol->permissions($detailed);
+		}
+		return $roles;
+
+	}
+
+
+}
